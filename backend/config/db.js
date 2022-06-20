32d22707f566e4b1c7b9451ec0bb309ba6b6f079
elton lima <eltonathayde2@gmail.com>
2022-06-20 09:52:20 -0300

    conexao com banco de dados mongodb atlas finalizada

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,25 @@
+// importação do middleware
 const mongoose = require("mongoose");
 
-// connection
+// puxando os dados do usuario do "dotenv"
+const dbUser = process.env.DB_USER
+const dbPassaword = process.env.DB_PASS
+
+
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassaword}@cluster0.eh1gm.mongodb.net/?retryWrites=true&w=majority`);
+
+        console.log("Conectou ao banco!");
+
+
+        return dbConn;
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+ conn();
+
+ module.exports = conn;
+

47ccda9383e67791abd466b7ccd8c85735237738
elton lima <eltonathayde2@gmail.com>
2022-06-22 09:27:58 -0300

    cricao de validacao de autenticacao

---
+++ backend/middlewares/authGuard.js
@@ -0,0 +1,29 @@
+// importação dos middlewares e bibliotecas
+
+const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+const jwtSecret = process.env.JWT_SECRET
+
+
+const authGuard = async (req,res,next) => {
+
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(" ")[1];
+
+//checagem se o cabecalho da requisição tem o token
+
+if(!token) return res.status(401).json({erros:["Acesso negado!"]})
+
+// checando se o token é valido 
+try {
+    
+    const verifed = jwt.verify(token,jwtSecret)
+
+    req.user = await  User.findByIdVE(verifed.id).select("-password");
+
+    next();
+    
+} catch (error) {
+    res.status(401).json({erros:["Token inválido."]})
+}
+}

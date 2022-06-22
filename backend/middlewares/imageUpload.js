31dea4c77c5bb345d17bf5ab869cfe67ce347c32
elton lima <eltonathayde2@gmail.com>
2022-06-22 10:03:31 -0300

    middleware de upload de imagem finalizado

---
+++ backend/middlewares/imageUpload.js
@@ -0,0 +1,41 @@
+//  importando bibliotecas e middlewares.
+
+const multer = require("multer")
+const path = require("path")
+
+// destino da imagem salva
+
+const imageStorage = multer.diskStorage({
+    // configurando o distino da imagem
+    destination: ( req ,file, cb) => {
+        let folder = ""
+
+        if(req.baseUrl.includes("users")){
+            folder = "users"  
+        }else if(req.baseUrl.includes("photos")){
+            folder = "photos"
+        }
+
+        cb(null,`uploads/${folder}/`)
+    },
+    // nome do arquivo padrão
+    filename: (req,file ,cb) => {
+
+        cb(null,Date.now() + path.extname(file.originalname))
+    }
+})
+
+
+const imagemUpload= multer({
+    storage:imageStorage,
+    // verificando o tipo de extensão da imagem
+    fileFilter(req,file ,cb){
+        if(!file.originalname.match(/\.(png|jpg)$/)){
+            
+            //apenas uploads em formatos png ou jpg 
+            return cb(new Error("Por favor, envie apenas png ou jpg!"))
+        }
+        cb(undefined,true)
+    }
+})
+module.exports = {imagemUpload};

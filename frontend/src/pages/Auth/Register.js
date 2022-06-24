b3bdbc06b7ac9fd9d4bfb854eb88696b791199dd
elton lima <eltonathayde2@gmail.com>
2022-06-24 10:35:05 -0300

    criacao do states do form inicados

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -8,9 +8,22 @@ import { useState, useEffect } from "react"
 
 
 const Register = () => {
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [confirmPassword, setConfimPassword] = useState("")
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+    console.log(user);
   }
 
   return (
@@ -18,10 +31,10 @@ const Register = () => {
       <h2>FanáticosGran</h2>
       <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome"/>
-        <input type="email" placeholder="E-email"/>
-        <input  type="password" placeholder="Senha"/>
-        <input type="password" placeholder="Confirme sua senha"/>
+        <input type="text" placeholder="Nome" onChange={(e) => setName(e.target.value)} value={name || ""}/>
+        <input type="email" placeholder="E-email" onChange={(e) => setEmail(e.target.value)} value={email || ""}/>
+        <input  type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password || ""}/>
+        <input type="password" placeholder="Confirme sua senha" onChange={(e) => setConfimPassword(e.target.value)} value={confirmPassword || ""}/>
         <input type="submit" value="Cadastrar"/>
       </form>
       <p>

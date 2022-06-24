97f3c6424ae142b9fe0353397fe00489fafa442d
elton lima <eltonathayde2@gmail.com>
2022-06-24 09:43:44 -0300

    criação do formulario de cadastro

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,8 +1,33 @@
 import "./Auth.css"
 
+// Components
+import {Link} from "react-router-dom"
+
+// Hooks 
+import { useState, useEffect } from "react"
+
+
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGran</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome"/>
+        <input type="email" placeholder="E-email"/>
+        <inpt  type="password" placeholder="Senha"/>
+        <input type="password" placeholder="Confirme sua senha"/>
+        <input type="submit" value="Cadastrar"/>
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>
   )
 }
 

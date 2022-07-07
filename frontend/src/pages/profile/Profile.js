d91e49eb87edba09ad3db40d888836a021dccec4
elton lima <eltonathayde2@gmail.com>
2022-07-07 14:33:59 -0300

    exibindo dados do usuario pelo id

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -15,11 +15,40 @@ import { useSelector,useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 
 // redux
+import { getUserDetails } from "../../slice/userSlice"
 
 
 const Profile = () => {
+     const {id} = useParams()
+
+     const dispatch = useDispatch()
+
+     const {user,loading} = useSelector((state) => state.user)
+     const {user: userAuth} = useSelector((state)=> state.auth)
+
+    //  photo
+
+    // carregando dados do usuario
+    useEffect(()=>{ 
+        dispatch(getUserDetails(id))
+    }, [dispatch,id])
+
+    if (loading){
+        return <p>Carregando...</p>
+    }
+    
   return (
-    <div>Profile</div>
+    <div id="profile">
+        <div className="profile-header">
+            {user.profileImage && (
+                <img src={`${uploads}/users/${user.profileImage}`} alt ={user.name} />
+            )}
+            <div className="profile-description">
+                <h2>{user.name}</h2>
+                <p>{user.bio}</p>
+            </div>
+        </div>
+    </div>
   )
 }
 

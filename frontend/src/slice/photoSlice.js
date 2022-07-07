b789994daf2a09926b31fbdeeeb072e22afe7a9b
elton lima <eltonathayde2@gmail.com>
2022-07-07 15:52:19 -0300

    Slice de insercao de fotos

--- frontend/src/slice/photoSlice.js
+++ frontend/src/slice/photoSlice.js
@@ -9,6 +9,26 @@ const initialState ={
     loading:false,
     message:null,
 }
+// publicar fotos do usuario
+ export const publishPhoto = createAsyncThunk(
+    "photo/publish",
+    async(photo,thunkAPI) => {
+    
+        const token = thunkAPI.getState().auth.user.token
+
+
+        const data = await photoService.publishPhoto(photo,token)
+
+
+        // checagem de erros 
+
+        if(data.errors){
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data;
+    }
+ )
 
 export const photoSlice= createSlice({
     name:"photo",
@@ -17,6 +37,26 @@ export const photoSlice= createSlice({
         resetMessage: (state) => {
             state.message = null;
         }
+    },
+    extraReducers:(builder) =>{
+        builder
+        .addCase(publishPhoto.pending, (state) =>{
+            state.loading = true;
+            state.error = false
+        })
+        .addCase(publishPhoto.fulfilled, (state, action) => {
+            state.loading = false;
+            state.sucess = true;
+            state.error = null;
+            state.user = action.payload;
+            state.photos.unshift(state.photo)
+            state.message = "Foto puclicadad com sucesso!"
+        })
+        .addCase(publishPhoto.rejected, (state,action) => {
+            state.loading = false;
+            state.error = action.payload
+            state.photo = {};
+        })
     }
 })
 

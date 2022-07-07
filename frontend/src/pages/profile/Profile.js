bec1e5b576b96aa65a8731cce2447dc7b0aa3c90
elton lima <eltonathayde2@gmail.com>
2022-07-07 14:03:09 -0300

    criando pagina de postagem de fotos

---
+++ frontend/src/pages/profile/Profile.js
@@ -0,0 +1,26 @@
+import "./Profile.css"
+
+import { uploads } from "../../utils/config"
+
+// component
+import Message from "../../components/Message"
+import { Link } from "react-router-dom"
+import { BsFillEyeFill,BsPencilFill,BsXlg } from "react-icons/bs"
+
+
+// hooks
+
+import { useState,useEffect, useRef} from "react"
+import { useSelector,useDispatch } from "react-redux"
+import { useParams } from "react-router-dom"
+
+// redux
+
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile

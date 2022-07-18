f68a6c6654f899e20f2f796a8b4a0b2182365c88
elton lima <eltonathayde2@gmail.com>
2022-07-18 16:19:31 -0300

    criacao de pagina individual de  foto

---
+++ frontend/src/pages/Photo/Photo.js
@@ -0,0 +1,25 @@
+import "./Photo.css"
+
+import { uploads } from "../../utils/config"
+
+//  components
+
+import Message from "../../components/Message"
+import { Link } from "react-router-dom"
+
+
+// hooks
+
+import { useEffect , useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { useParams } from "react-router-dom"
+
+// redux
+
+const Photo = () => {
+  return (
+    <div>Photo</div>
+  )
+}
+
+export default Photo

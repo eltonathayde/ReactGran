460508d0f89dca78c8ae451ec721653d332f1bc4
elton lima <eltonathayde2@gmail.com>
2022-07-25 11:32:11 -0300

    service de like finalizado

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -79,13 +79,32 @@ const getPhoto = async (id, token) => {
     }
 }
 
+// fução de like 
+
+const like = async(id,token) => {
+
+    const config = requestConfig("PUT", null, token)
+
+    try {
+        
+        const res = await fetch(api + "/photos/like/" + id, config).then((res)=> res.json()).catch((err) => err)
+        
+        return res
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
 
 const photoService = {
     publishPhoto,
     getUserPhotos,
     deletePhoto,
     updatePhoto,
-    getPhoto
+    getPhoto,
+    like
 }
 
 export default photoService;
